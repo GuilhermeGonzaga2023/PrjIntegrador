d049ffc246f8bede7f24b9f5bbcb1c1069d572e4
felipebioni <felipereisbioni@gmail.com>
2023-03-10 22:26:13 -0300

    primeira alteracao banco de dados

---
+++ SqlBanco.sql
@@ -0,0 +1,24 @@
+﻿create table Cliente
+(
+idCliente int identity(1,1),
+nomeCliente varchar(100) not null,
+loginCliente varchar(50)not null,
+senhaCliente varchar(50)not null
+constraint pk_Cliente primary key (idCliente)
+)
+
+create table Funcionario
+(
+idFuncionario int identity(1,1),
+nomeFuncionario varchar(100)not null,
+cpfFuncionario char(11)not null,
+loginFuncionario varchar(50)not null,
+senhaFuncionario varchar(50)not null
+constraint pk_funcioario primary key (idFuncionario)
+)
+
+create table Vagas
+(
+idVaga int identity(1,1),
+numeroVaga int
+)
